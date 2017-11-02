3ca4e45a123d80447cc174e571a095426451f423
ElRocha <zepedro9753@gmail.com>
2017-11-02 16:13:03 +0000

    Criação do ficheiro main.

---
+++ pyGoCompiler.py
@@ -0,0 +1,22 @@
+from plintax import *
+
+def main():
+    file = True
+    if not file:
+        while True:
+            try:
+                s = input('calc > ')
+            except EOFError:
+                print()
+                break
+            if not s:
+                continue
+            result = parser.parse(s)
+            if result is not None:
+                print(result)
+    else:
+        fd = open("example1.go", "r")
+        result = parser.parse(''.join(fd.readlines()))
+
+if __name__ == '__main__':
+    main()
