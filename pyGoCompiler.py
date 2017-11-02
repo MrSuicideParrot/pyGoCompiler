8c260451caff238f88e9fa40755ecc6bd52f4a57
ElRocha <zepedro9753@gmail.com>
2017-11-02 19:12:06 +0000

    Parse dos argumentos está bonito para o cirne ficar contente.

--- pyGoCompiler.py
+++ pyGoCompiler.py
@@ -1,19 +1,17 @@
 from plintax import parser
-import argparse
-import sys
+from argparse import ArgumentParser
 
 def main():
-    argsParser = argparse.ArgumentParser()
-    argsParser.add_argument('-tree', nargs=1, required=True)
-    argsParser.parse_args()
-    arg=str(sys.argv[2])
+    argsParser = ArgumentParser(description='Compilador de código go em python.')
+    argsParser.add_argument('-t', '--tree', nargs=1, type=str, dest='file', required=True)
+    args=argsParser.parse_args()
+    arg=args.file[0]
     if arg[-3:] == '.go':
         fd = open(arg, "r")
         result = parser.parse(''.join(fd.readlines()))
         result.pprint()
     else:
         print('O ficheiro escolhido não é código go.')
-
 
 if __name__ == '__main__':
     main()
