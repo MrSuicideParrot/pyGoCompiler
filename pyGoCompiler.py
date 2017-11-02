bf35f6f7cff1b5fc71f54a331ddf54938c542096
André Cirne <ancirne@gmail.com>
2017-11-02 20:53:33 +0000

    Afinal o parse não estava assim tão bonito...
    O cirne não ficou contente e teve d eo refazer

--- pyGoCompiler.py
+++ pyGoCompiler.py
@@ -1,17 +1,26 @@
 from plintax import parser
 from argparse import ArgumentParser
+from os.path import isfile
+
 
 def main():
     argsParser = ArgumentParser(description='Compilador de código go em python.')
-    argsParser.add_argument('-t', '--tree', nargs=1, type=str, dest='file', required=True)
-    args=argsParser.parse_args()
-    arg=args.file[0]
-    if arg[-3:] == '.go':
-        fd = open(arg, "r")
-        result = parser.parse(''.join(fd.readlines()))
-        result.pprint()
-    else:
-        print('O ficheiro escolhido não é código go.')
+
+    argsParser.add_argument('-p', '--print_tree', action='store_true', help='Print the abstract tree')
+    argsParser.add_argument('-f', '--file', type=str, required=True, help='File')
+
+    args = argsParser.parse_args()
+
+    if not isfile(args.file):
+        print('ERRO: Ficheiro inexistente!')
+        exit(1)
+
+    with open(args.file, "r") as fd:
+        abstract_tree = parser.parse(''.join(fd.readlines()))
+
+    if args.print_tree:
+        abstract_tree.pprint()
+
 
 if __name__ == '__main__':
     main()
