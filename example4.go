6517e753c737dd94f0694aa279245166e4135316
Unknown <zepedro9753@gmail.com>
2017-12-10 00:55:07 +0000

    Auto stash before merge of "master" and "origin/master"

--- example4.go
+++ example4.go
@@ -3,13 +3,20 @@ package main
 import "fmt"
 
 func main() {
+<<<<<<< Updated upstream
     fmt.Scan(a);
     b:=0;
     c:=1;
+=======
+	fmt.Scan(a);
+	b:=0;
+	c:=1;
+>>>>>>> Stashed changes
     temp:=0;
 
     fmt.Print(b);
     fmt.Print(c);
+<<<<<<< Updated upstream
     for i:=0; i<a; i++ {
         temp=c;
         c=c+b;
@@ -18,3 +25,12 @@ func main() {
     }
 }
 
+=======
+	for i:=0; i<a; i++ {
+	    temp=c;
+	    c=c+b;
+	    b=temp;
+	    fmt.Print(c);
+	}
+}
+>>>>>>> Stashed changes
